698e45a082272221865d032c154c53bc89b1da10
Sennett Lau <laub1199@gmail.com>
2023-12-15 00:17:06 +0800

    updated Footer

--- src/component/common/Footer.tsx
+++ src/component/common/Footer.tsx
@@ -1,17 +1,48 @@
-import { Flex, Image, Link, Text } from '@chakra-ui/react'
+import { Flex, Text } from '@chakra-ui/react'
+import Highlight from '../common/Highlight'
 
 const Footer = () => {
   return (
-    <Flex justifyContent={'center'} alignItems={'center'} width={'100%'} height={'50px'} bg={'gray.50'}>
-      <Text>
-        Next Chakra Starter | By
-      </Text>
-      <Link href={'https://0xblanc.io'} isExternal={true} color={'blanc.200'}>
-        <Flex alignItems={'center'}>
-          <Image src={'/assets/logo.svg'} alt={'logo'} width={'20px'} height={'20px'} ml={2} mr={1} />
-          0xBlanc
+    <Flex w={'100%'} bg={'themeLight.500'}>
+      <Flex
+        w={'100%'}
+        maxW={'1280px'}
+        my={'24px'}
+        mx={'auto'}
+        alignContent={'center'}
+      >
+        <Flex flex={1} justifyContent={'flex-start'}>
+          <Text
+            fontSize={'20px'}
+            lineHeight={'20px'}
+            color={'themeDark.500'}
+            fontFamily={'Zarathustra'}
+          >
+            <Highlight>Sennett Lau.</Highlight>© 2023
+          </Text>
         </Flex>
-      </Link>
+        <Flex flex={1} justifyContent={'center'}>
+          <Text
+            fontSize={'16px'}
+            lineHeight={'16px'}
+            color={'themeDark.500'}
+            fontFamily={'Zarathustra'}
+          >
+            Site designed and developed by Sennett Lau
+          </Text>
+        </Flex>
+        <Flex flex={1} justifyContent={'flex-end'}>
+          <Text
+            fontSize={'20px'}
+            lineHeight={'20px'}
+            color={'themeDark.500'}
+            fontFamily={'Zarathustra'}
+            fontWeight={'bold'}
+          >
+            Back to top
+          </Text>
+        </Flex>
+      </Flex>
     </Flex>
   )
 }
