2b0089bd68349fe9dfdba397db6d50adcf037791
Sennett Lau <laub1199@gmail.com>
2023-12-19 01:18:29 +0800

    added animation for IndexAbout

--- src/component/index/IndexAbout/IndexAbout.tsx
+++ src/component/index/IndexAbout/IndexAbout.tsx
@@ -1,15 +1,52 @@
 import Highlight from '@/component/common/Highlight'
 import { RootState } from '@/store'
-import { getBackgroundColorScheme, getContentColorScheme, getSquareColorScheme, range } from '@/utils'
+import {
+  getBackgroundColorScheme,
+  getContentColorScheme,
+  getSquareColorScheme,
+  range,
+} from '@/utils'
 import { Box, Flex, Image, Text } from '@chakra-ui/react'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 
 const IndexAbout: React.FC = () => {
   const colorScheme = useSelector(
     (state: RootState) => state.controlSlice.colorScheme,
   )
-  
+  const currSectionId = useSelector(
+    (state: RootState) => state.controlSlice.currSectionId,
+  )
+
+  const [isAnimationTriggered, setIsAnimationTriggered] = useState(false)
+  const [transitionStep, setTransitionStep] = useState(0)
+
+  useEffect(() => {
+    if (currSectionId === 'about' && !isAnimationTriggered) {
+      setIsAnimationTriggered(true)
+
+      // 1 - 3: 3x 'About.' display
+      // 2 - 9: content display
+      // 10: bottom right 'About.' display - 'A'
+      // for bottom right 'About.' display
+      // 11 - 15: for bottom right 'About.' display - b o u t .
+
+      setTransitionStep(1)
+
+      for (let i = 2; i <= 10; i++) {
+        setTimeout(() => {
+          setTransitionStep(i)
+        }, 250 * (i - 1))
+      }
+
+      for (let i = 11; i <= 15; i++) {
+        setTimeout(() => {
+          setTransitionStep(i)
+        }, 250 * 9 + 200 * (i - 10))
+      }
+    }
+  }, [currSectionId, isAnimationTriggered])
+
   return (
     <Flex
       id='about'
@@ -56,24 +93,34 @@ const IndexAbout: React.FC = () => {
             transformOrigin: 'bottom right',
           }}
         >
-          <Text
-            fontSize={'96px'}
-            fontWeight={700}
-            h={'106px'}
-            mr={'30px'}
-          >
-            About
+          <Text fontSize={'96px'} fontWeight={700} h={'106px'} mr={'30px'}>
+            {'About'.split('').map((c, i) => (
+              <Text
+                as={'span'}
+                key={i}
+                opacity={transitionStep >= i + 10 ? 1 : 0}
+                transition={'all 0.3s ease-in-out'}
+              >
+                {c}
+              </Text>
+            ))}
           </Text>
-          <Image src={getSquareColorScheme(colorScheme)} />
+          <Image
+            src={getSquareColorScheme(colorScheme)}
+            opacity={transitionStep >= 15 ? 1 : 0}
+            transition={'all 0.3s ease-in-out'}
+          />
         </Flex>
         <Flex gap={'22px'} h={'fit-content'}>
           {range(0, 3).map((i) => (
-            <Flex key={i} gap={'33px'} alignItems={'end'}>
-              <Text
-                fontSize={'96px'}
-                fontWeight={700}
-                h={'106px'}
-              >
+            <Flex
+              key={i}
+              gap={'33px'}
+              alignItems={'end'}
+              opacity={transitionStep >= i + 1 ? 1 : 0}
+              transition={'all 0.5s ease-in-out'}
+            >
+              <Text fontSize={'96px'} fontWeight={700} h={'106px'}>
                 About
               </Text>
               <Image src={getSquareColorScheme(colorScheme)} />
@@ -95,38 +142,67 @@ const IndexAbout: React.FC = () => {
             lineHeight={'28px'}
             gap={'24px'}
           >
-            <Text>
+            <Text
+              h={'28px'}
+              opacity={transitionStep >= 4 ? 1 : 0}
+              transform={transitionStep >= 4 ? 'none' : 'translateX(-20px)'}
+              transition={'all 0.5s ease-in-out'}
+            >
               I'm <Highlight>Sennett Lau</Highlight>, a full-stack developer
               based in Hong Kong.
             </Text>
-            <Text>
+            <Text
+              h={'84px'}
+              opacity={transitionStep >= 5 ? 1 : 0}
+              transform={transitionStep >= 5 ? 'none' : 'translateX(-20px)'}
+              transition={'all 0.5s ease-in-out'}
+            >
               Holding a Bachelor of Science in{' '}
               <Highlight>Computer Science</Highlight> with a focus on Artificial
               Intelligence and Data Science from the{' '}
               <Highlight>City University of Hong Kong</Highlight>, graduated on{' '}
               <Highlight>June, 2022</Highlight>.
             </Text>
-            <Text>
+            <Text
+              h={'112px'}
+              opacity={transitionStep >= 6 ? 1 : 0}
+              transform={transitionStep >= 6 ? 'none' : 'translateX(-20px)'}
+              transition={'all 0.5s ease-in-out'}
+            >
               With a strong foundation in <Highlight>web development</Highlight>{' '}
-              specialized in
-              <Highlight>JavaScript</Highlight>, a year of experience in{' '}
-              <Highlight>Solidity</Highlight> and <Highlight>Web3</Highlight>,
-              and growing proficiency in <Highlight>cloud computing</Highlight>,
-              particularly <Highlight>AWS</Highlight>,{' '}
-              <Highlight>GoLang</Highlight> backend application and{' '}
-              <Highlight>system design</Highlight>.
+              specialized in <Highlight>JavaScript</Highlight>, a year of
+              experience in <Highlight>Solidity</Highlight> and{' '}
+              <Highlight>Web3</Highlight>, and growing proficiency in{' '}
+              <Highlight>cloud computing</Highlight>, particularly{' '}
+              <Highlight>AWS</Highlight>, <Highlight>GoLang</Highlight> backend
+              application and <Highlight>system design</Highlight>.
             </Text>
-            <Text>
+            <Text
+              h={'56px'}
+              opacity={transitionStep >= 7 ? 1 : 0}
+              transform={transitionStep >= 7 ? 'none' : 'translateX(-20px)'}
+              transition={'all 0.5s ease-in-out'}
+            >
               Technically proficient in <Highlight>TypeScript</Highlight>,{' '}
               <Highlight>Terraform</Highlight>,{' '}
               <Highlight>Kubernetes</Highlight>, <Highlight>ReactJS</Highlight>,
               and more.
             </Text>
-            <Text>
+            <Text
+              h={'28px'}
+              opacity={transitionStep >= 8 ? 1 : 0}
+              transform={transitionStep >= 8 ? 'none' : 'translateX(-20px)'}
+              transition={'all 0.5s ease-in-out'}
+            >
               I am a <Highlight>fast learner</Highlight> and an effective{' '}
               <Highlight>team player</Highlight>.
             </Text>
-            <Text>
+            <Text
+              h={'56px'}
+              opacity={transitionStep >= 9 ? 1 : 0}
+              transform={transitionStep >= 9 ? 'none' : 'translateX(-20px)'}
+              transition={'all 0.5s ease-in-out'}
+            >
               I'm dedicated to pushing the envelope in tech, blending a solid
               technical base with a keen interest in emerging technologies.
             </Text>
