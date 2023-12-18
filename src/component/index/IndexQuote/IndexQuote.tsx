e64e22219a7749a3e97a75ddfc0062e9cfe3d850
Sennett Lau <laub1199@gmail.com>
2023-12-18 18:03:49 +0800

    added animation for IndexQuote

--- src/component/index/IndexQuote/IndexQuote.tsx
+++ src/component/index/IndexQuote/IndexQuote.tsx
@@ -1,12 +1,35 @@
 import { RootState } from '@/store'
 import { getBackgroundColorScheme, getContentColorScheme } from '@/utils'
 import { Box, Flex, Text } from '@chakra-ui/react'
+import { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
+
+const quote =
+  'I develop digital experiences that inspire, infused with cutting-edge technology.'
 
 const IndexQuote = () => {
   const colorScheme = useSelector(
     (state: RootState) => state.controlSlice.colorScheme,
   )
+  const currSectionId = useSelector(
+    (state: RootState) => state.controlSlice.currSectionId,
+  )
+
+  const [characterIndex, setCharacterIndex] = useState(0)
+  const [isAnimationTriggered, setIsAnimationTriggered] = useState(false)
+
+  useEffect(() => {
+    if (currSectionId === 'quote' && !isAnimationTriggered) {
+      const interval = setInterval(() => {
+        setCharacterIndex((characterIndex) => characterIndex + 1)
+
+        if (characterIndex === quote.length) {
+          clearInterval(interval)
+        }
+      }, 50)
+      setIsAnimationTriggered(true)
+    }
+  }, [currSectionId, isAnimationTriggered])
 
   return (
     <Flex
@@ -36,8 +59,12 @@ const IndexQuote = () => {
           justifyContent={'center'}
         >
           <Text fontWeight={'medium'} fontSize={'64px'} fontStyle={'italic'}>
-            I develop digital experiences that inspire, infused with
-            cutting-edge technology.
+            {quote
+              .split('')
+              .slice(
+                0,
+                characterIndex > quote.length ? quote.length : characterIndex,
+              )}
           </Text>
         </Flex>
         <Flex flexDir={'column'} justifyContent={'flex-end'}>
