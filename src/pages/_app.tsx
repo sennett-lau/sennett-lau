3c702a72ace852e7208f50761c0894ba62bf7ae7
Sennett Lau <laub1199@gmail.com>
2023-12-19 00:53:12 +0800

    uninstall aos

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,20 +4,10 @@ import '@/styles/font.scss'
 import '@/styles/globals.scss'
 import customTheme from '@/styles/theme'
 import { ChakraProvider } from '@chakra-ui/react'
-import AOS from 'aos'
-import 'aos/dist/aos.css'
 import type { AppProps } from 'next/app'
-import { useEffect } from 'react'
 import { Provider } from 'react-redux'
 
 const App = ({ Component, pageProps }: AppProps) => {
-  useEffect(() => {
-    AOS.init({
-      easing: 'ease-out-cubic',
-      once: true,
-      offset: 0,
-    })
-  }, [])
   return (
     <ChakraProvider theme={customTheme}>
       <Provider store={store}>
