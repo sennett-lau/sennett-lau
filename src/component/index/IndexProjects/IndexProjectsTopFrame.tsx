72b1bde927820c84ca141a950dd8d835cb56d4ff
Sennett Lau <laub1199@gmail.com>
2023-12-23 23:27:02 +0800

    added animation for IndexProjectTopFrame

--- src/component/index/IndexProjects/IndexProjectsTopFrame.tsx
+++ src/component/index/IndexProjects/IndexProjectsTopFrame.tsx
@@ -1,13 +1,25 @@
 import { RootState } from '@/store'
 import { getContentColorScheme, getSquareColorScheme } from '@/utils'
 import { Box, Image } from '@chakra-ui/react'
+import { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 
 const IndexProjectsTopFrame = () => {
   const colorScheme = useSelector(
     (state: RootState) => state.controlSlice.colorScheme,
   )
-  
+  const currSectionId = useSelector(
+    (state: RootState) => state.controlSlice.currSectionId,
+  )
+
+  const [transitionStep, setTransitionStep] = useState(0)
+
+  useEffect(() => {
+    if (currSectionId === 'projects' && transitionStep === 0) {
+      setTransitionStep(1)
+    }
+  }, [currSectionId, transitionStep])
+
   return (
     <Box w={'100%'} position={'relative'} h={'74px'}>
       <Image
@@ -15,16 +27,19 @@ const IndexProjectsTopFrame = () => {
         position={'absolute'}
         top={'0'}
         left={'0'}
+        transform={transitionStep >= 1 ? 'scale(1)' : 'scale(6.5)'}
+        transition={'all 1.0s ease-in-out'}
       />
       <Box
-        w={'44px'}
-        h={'44px'}
+        w={transitionStep >= 1 ? '44px' : '1000px'}
+        h={transitionStep >= 1 ? '44px' : '0'}
         borderRight={'1px solid'}
         borderTop={'1px solid'}
         borderColor={getContentColorScheme(colorScheme)}
         position={'absolute'}
         top={'0'}
         right={'0'}
+        transition={'all 1.5s ease-in-out'}
       />
     </Box>
   )
