fcca9c8b24b7a5df1498b4d0a26a156e1d698de1
Sennett Lau <laub1199@gmail.com>
2023-12-29 01:59:20 +0800

    added more colored icons

--- src/component/index/IndexContact/IndexContactDigitalSpaces.tsx
+++ src/component/index/IndexContact/IndexContactDigitalSpaces.tsx
@@ -1,19 +1,34 @@
+import { RootState } from '@/store'
+import { getIconColorScheme } from '@/utils'
 import { Flex, Image, Text } from '@chakra-ui/react'
+import { useSelector } from 'react-redux'
 
 const IndexContactDigitalSpaces = () => {
+  const colorScheme = useSelector(
+    (state: RootState) => state.controlSlice.colorScheme,
+  )
+
   return (
     <Flex w={'fit-content'} flexDir={'column'} gap={'22px'}>
       <Text fontSize={'36px'} fontWeight={'bold'} lineHeight={'36px'}>
         Other Digital Spaces
       </Text>
       <Flex w={'fit-content'} gap={'8px'} alignItems={'center'}>
-        <Image src={'/assets/icons/linkedin.svg'} w={'24px'} h={'24px'} />
+        <Image
+          src={getIconColorScheme('linkedin', colorScheme)}
+          w={'24px'}
+          h={'24px'}
+        />
         <Text fontSize={'24px'} lineHeight={'24px'}>
           sennett-lau
         </Text>
       </Flex>
       <Flex w={'fit-content'} gap={'8px'} alignItems={'center'}>
-        <Image src={'/assets/icons/github.svg'} w={'24px'} h={'24px'} />
+        <Image
+          src={getIconColorScheme('github', colorScheme)}
+          w={'24px'}
+          h={'24px'}
+        />
         <Text fontSize={'24px'} lineHeight={'24px'}>
           sennett-lau
         </Text>
