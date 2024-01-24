dfdb2f904a7421f02b95ccfd104279c508848984
Sennett Lau <laub1199@gmail.com>
2024-01-24 17:40:17 +0800

    updated IndexProjectCityUGE with rwd

--- src/component/index/IndexProjects/IndexProjectCityUGE.tsx
+++ src/component/index/IndexProjects/IndexProjectCityUGE.tsx
@@ -11,8 +11,14 @@ const IndexProjectCityUGE = () => {
     <IndexProjectItemFrame
       year={2022}
       image={'/assets/projects/cityu-ge.png'}
-      imgW={'470px'}
-      imgH={'540px'}
+      imgW={{
+        base: 'auto',
+        lg: '470px',
+      }}
+      imgH={{
+        base: 'auto',
+        lg: '540px',
+      }}
       name={'CityU GE Guide'}
       description={'Frontend, Backend Development'}
       stack={'NuxtJs · TailwindCSS · Fastify · MongoDB'}
