fd7535c846c03b056283eea6c79253969c0df8b2
Sennett Lau <laub1199@gmail.com>
2024-02-01 17:36:56 +0800

    added CNAME related

--- export.sh
+++ export.sh
@@ -1,10 +1,12 @@
 mkdir temp
+cp ./docs/CNAME ./temp/CNAME
 cp ./docs/.nojekyll ./temp/.nojekyll
 cp ./docs/robots.txt ./temp/robots.txt
 npm run fmt
 npm run build
 rm -rf ./docs
 cp -rf ./out ./docs
+cp ./temp/CNAME ./docs/CNAME
 cp ./temp/.nojekyll ./docs/.nojekyll
 cp ./temp/robots.txt ./docs/robots.txt
 rm -rf ./temp
