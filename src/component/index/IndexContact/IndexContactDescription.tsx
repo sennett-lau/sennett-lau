be79b7aa0702a6ed072169a0aa2c6a9a0403967d
Sennett Lau <laub1199@gmail.com>
2024-02-01 16:22:22 +0800

    updated IndexContactDescription with rwd

--- src/component/index/IndexContact/IndexContactDescription.tsx
+++ src/component/index/IndexContact/IndexContactDescription.tsx
@@ -2,16 +2,19 @@ import { Flex, Text } from '@chakra-ui/react'
 
 const IndexContactDescription = () => {
   return (
-    <Flex w={'100%'} flexDir={'column'}>
-      <Text fontSize={'40px'} fontWeight={'bold'} mb={'0'} lineHeight={'40px'}>
-        Freelance? Job?
-      </Text>
-      <Text fontSize={'40px'} fontWeight={'bold'} mb={'0'} lineHeight={'40px'}>
-        Or any project idea?
-      </Text>
-      <Text fontSize={'40px'} fontWeight={'bold'} mb={'0'} lineHeight={'40px'}>
-        Do not hesitate to contact me!
-      </Text>
+    <Flex
+      w={'100%'}
+      flexDir={'column'}
+      fontSize={{
+        base: '32px',
+        lg: '40px',
+      }}
+      fontWeight={'bold'}
+      lineHeight={'40px'}
+    >
+      <Text mb={'0'}>Freelance? Job?</Text>
+      <Text mb={'0'}>Or any project idea?</Text>
+      <Text mb={'0'}>Do not hesitate to contact me!</Text>
     </Flex>
   )
 }
