b3c99c4a23eeaa389dfae145e360237b67a1585f
Sennett Lau <laub1199@gmail.com>
2025-02-27 03:23:59 +0800

    build

--- docs/_next/static/Z-MA76aYmW4ZlpfVqS14-/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-72f78248974ef2ce.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
